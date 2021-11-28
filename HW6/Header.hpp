76d05a54069f150df01b957a4bf856e0133afafe
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-11-29 00:20:03 +0300

    Update Header.hpp

--- HW6/Header.hpp
+++ HW6/Header.hpp
@@ -7,25 +7,28 @@ class Fractions
 {
 private:
 	int m_numerator;
-	std::size_t m_denominator;
+	int m_denominator;
 public:
-	Fractions() : m_numerator(0), m_denominator(1) {}
+	//Fractions() : m_numerator(0), m_denominator(1) {}
 
-	Fractions(int num, unsigned den = 1) : m_numerator(num), m_denominator(den) 
-	{
-		if (den == 0)
-		{
-			std::cerr << "Zero denominator";
-		}
-	}
+	//Fractions(int num, int den = 1) : m_numerator(num), m_denominator(den) 
+	//{
+	//	if (den == 0)
+	//	{
+	//		std::cerr << "Zero denominator";
+	//	}
+	//}
+
+	Fractions(const Fractions &) = default;
+
 	~Fractions();
 
 	int get_numerator() { return m_numerator; }
-	unsigned get_dedominator() { return m_denominator; }
+	int get_dedominator() { return m_denominator; }
 
 	void reduction();
 
-	double converting();
+	operator double();
 
 	friend std::ostream& operator<< (std::ostream& stream, const Fractions& fraction); /* перегрузку ввода и вывода реализуем через дружественные функции, т.к. левый операнд не является эземпляром класса Fractions*/
 	friend std::istream& operator>> (std::istream& stream, Fractions& fraction);
