a6c6acc94f09c506c4342b72104e28c1452a68dd
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-11-29 23:21:27 +0300

    Update Num1.cpp

--- HW9/Num1.cpp
+++ HW9/Num1.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+// В качестве такой сортировки (сложностью O(nlogn)) подходит быстрая сортировка quick sort
 
-template < typename T >
-void quick_sort(T* arr, int left, int right)
+#include <iostream>
+#include <vector>
+
+template < typename T, typename F >
+void quick_sort(T* arr, int left, int right, F compare)
 {
 	if (right - left < 1) { return };
 
@@ -9,13 +12,26 @@ void quick_sort(T* arr, int left, int right)
 
 	for (auto i = left; i < right; i++)
 	{
-		if arr[i] < arr[right]
+		if compare(arr[i], arr[right])
 		{
 			std::swap(arr[i], arr[k]);
 			k++;
 		}
 	}
 	std::swap(arr[k], arr[right]);
-	quick_sort(arr, left, k - 1);
-	quick_sort(arr, k + 1, right);
+	quick_sort(arr, left, k - 1, compare);
+	quick_sort(arr, k + 1, right, compare);
+}
+
+
+template< typename T, std::size_t N, typename F > // статический массив
+void sort(T (&arr) [N], F compare)
+{
+	quick_sort(arr, 0, N - 1, F compare);
+}
+
+template < typename T, typename F  > // динамический массив
+void sort(T* arr, std::size_t size, F compare)
+{
+	quick_sort(arr, 0, size - 1, F compare);
 }
