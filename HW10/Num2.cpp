6ae93be128bd0ed4a9e83aebb31ea8d38e33117b
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-11-29 05:32:20 +0300

    Update Num2.cpp

--- HW10/Num2.cpp
+++ HW10/Num2.cpp
@@ -7,10 +7,10 @@ Type* ptr(Types ... args) { return new Type(args...); }
 
 int main()
 {
-	auto* pointer = ptr < std::vector < double > > (3.141592, 2.721828);
-	std::cout << typeid(pointer).name() << "\n\n";
-	std::cout << typeid(*pointer).name() << "\n\n";
-	delete pointer;
+	auto* point = ptr < std::vector < double > > (3.141592, 2.721828);
+	std::cout << typeid(point).name() << "\n\n";
+	std::cout << typeid(*point).name() << "\n\n";
+	delete point;
 
 	return 0;
 }
