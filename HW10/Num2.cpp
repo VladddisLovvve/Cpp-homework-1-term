92ef2583112f521f8371b62a943f44d6bcb579a1
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-11-29 04:45:42 +0300

    Create Num2.cpp

---
+++ HW10/Num2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+
+template <typename Type, typename ... Types>
+Type* ptr(Types ... args) { return new Type(args...); }
+
+int main()
+{
+	auto* pointer = ptr< std::vector<double> >(3.141592, 2.721828);
+	std::cout << typeid(pointer).name() << '\n\n';
+	std::cout << typeid(*pointer).name();
+	delete pointer;
+
+	return 0;
+}
