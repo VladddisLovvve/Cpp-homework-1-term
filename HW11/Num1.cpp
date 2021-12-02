c466358bfcc706b673d3cda36ebfb7bf73d73356
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 03:03:47 +0300

    Update Num1.cpp

--- HW11/Num1.cpp
+++ HW11/Num1.cpp
@@ -2,20 +2,20 @@
 #include <type_traits>
 
 
-template < typename T, typename ... A >
+template < typename RT, typename ... A >
 struct is_function_v  : std::false_type {};
 
-template < typename T, typename ... A >
-struct is_function_v < T(A...) > : std::true_type {};
+template < typename RT, typename ... A >
+struct is_function_v < RT(A...) > : std::true_type {};
 
-template < typename T, typename ... A >
-struct is_function_v < const T(A...) > : std::true_type {};
+template < typename RT, typename ... A >
+struct is_function_v < const RT(A...) > : std::true_type {};
 
-template < typename T, typename ... A >
-struct is_function_v < T(A...) & > : std::true_type {};
+template < typename RT, typename ... A >
+struct is_function_v < RT(A...) & > : std::true_type {};
 
-template < typename T, typename ... A >
-struct is_function_v < T(A...) && > : std::true_type {};
+template < typename RT, typename ... A >
+struct is_function_v < RT(A...) && > : std::true_type {};
 
 
 
