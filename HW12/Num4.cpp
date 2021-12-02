bec7c3c04bbd407c6624714e469848f07ac029a2
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 23:37:13 +0300

    Update Num4.cpp

--- HW12/Num4.cpp
+++ HW12/Num4.cpp
@@ -1,27 +1,27 @@
 template < int num, int k >
-constexpr bool is_prime_loop(int num, int k) 
+constexpr bool is_prime_loop() 
 {
     return (k * k > num) ? true : (num % k == 0) ? false : is_prime_loop(num, k + 1);
 }
 
 template < int num >
-constexpr bool is_prime(int num)
+constexpr bool is_prime()
 {
 	return is_prime_loop(num, 2);
 }
 
 template < int num >
-constexpr int get_prime(int num)
+constexpr int get_prime()
 { return get_prime_loop(num, 2); }
 
 
 template < int num, int k >
-constexpr int get_prime_loop(int num, int k)
+constexpr int get_prime_loop()
 { return (num == 0) ? k : getPrimeLoop(num - 1, next_prime(k + 1)); }
 
 
 template < int k >
-constexpr int next_prime(int k)
+constexpr int next_prime()
 {
 	return is_prime(k) ? k : next_prime(k + 1);
 }
