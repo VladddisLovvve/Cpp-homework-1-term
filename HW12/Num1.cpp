cba5e79e05789898c11c71443f2f2d5bf0bf184e
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 22:35:16 +0300

    Update Num1.cpp

--- HW12/Num1.cpp
+++ HW12/Num1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+
+template < int n, int k > // C из n по k
+struct binomial_coeficcient
+{
+	static int ans = binomial_coeficcient < n - 1, k > ::ans +
+		binomial_coeficcient < n - 1, k - 1 > ::ans; // правило сложения биномиальных коэффициентов из теорвера
+};
+
+template < int n >
+struct binomial_coeficcient  < n, 0 > // C из n по 0 = 1
+{
+	static int ans = 1;
+};
+
+
+template < int n >
+struct binomial_coeficcient < n, n > // C из n по n = 1
+{
+	static int ans = 1;
+};
