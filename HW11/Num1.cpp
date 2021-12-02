2077e6f5f07c73ba1f368dd039409b0311582c45
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 03:34:03 +0300

    Update Num1.cpp

--- HW11/Num1.cpp
+++ HW11/Num1.cpp
@@ -3,25 +3,29 @@
 
 
 template < typename RT, typename ... A >
-struct is_function_v  : std::false_type {};
+struct is_function  : std::false_type {};
 
 template < typename RT, typename ... A >
-struct is_function_v < RT(A...) > : std::true_type {};
+struct is_function < RT(A...) > : std::true_type {};
 
 template < typename RT, typename ... A >
-struct is_function_v < const RT(A...) > : std::true_type {};
+struct is_function < const RT(A...) > : std::true_type {};
 
 template < typename RT, typename ... A >
-struct is_function_v < RT(A...) & > : std::true_type {};
+struct is_function < RT(A...) & > : std::true_type {};
 
 template < typename RT, typename ... A >
-struct is_function_v < RT(A...) && > : std::true_type {};
+struct is_function < RT(A...) && > : std::true_type {};
+
+
+template < typename RT, typename ... A >
+using is_function_v = typename is_function < RT(A...) > ::value
 
 
 
 int main()
 {
-	std::cout << is_function_v < int(bool) > ::value << std::endl;
-	std::cout << is_function_v < double(int) > ::value << std::endl;
+	std::cout << is_function_v < int(bool) > << std::endl;
+	std::cout << is_function_v < double(int) > << std::endl;
 	return 0;
 }
