eb17eb2221dd8b8df5ebf30a602a1fa51dea8965
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 20:58:21 +0300

    Update Num1.cpp

--- HW11/Num1.cpp
+++ HW11/Num1.cpp
@@ -6,41 +6,44 @@ template < typename RT, typename ... A >
 struct is_function : std::false_type {};
 
 template < typename RT, typename ... A >
-struct is_function < RT(A...) > : std::true_type {};
-
-template < typename RT, typename ... A >
-struct is_function < const RT(A...) > : std::true_type {};
-
-template < typename RT, typename ... A >
-struct is_function < RT(A...)& > : std::true_type {};
-
-template < typename RT, typename ... A >
-struct is_function < RT(A...)&& > : std::true_type {};
-
-
-template < typename RT >
-inline constexpr bool is_function_v = is_function < RT > ::value;
-
-template < typename RT >
-struct function_type 
+struct is_function < RT(A...) > : std::true_type
 {
 	using type = RT;
 };
 
+template < typename RT, typename ... A >
+struct is_function < const RT(A...) > : std::true_type 
+{
+	using type = RT;
+};
+
+template < typename RT, typename ... A >
+struct is_function < RT(A...)& > : std::true_type 
+{
+	using type = RT;
+};
+
+template < typename RT, typename ... A >
+struct is_function < RT(A...)&& > : std::true_type 
+{
+	using type = RT;
+};
+
+
 template <typename RT>
-using function_t = typename function_type < RT > ::type;
+using function_t = typename is_function < RT > ::type;
+//using function_t = typeid(typename function_type < RT > ::type).name();
 
-//using function_t = typeid(typename function_type < RT > ::type).name(); - такая реализация почему-то не компилируется
 
-void f() {}
-double g() { return 1.0; }
 
+template < typename RT >
+inline constexpr bool is_function_v = is_function < RT > ::value;
 
 
 
 int main()
 {
 	std::cout << is_function_v < int(bool) > << std::endl;
-	std::cout << typeid(function_t < double(int) >).name() << std::endl; //реализовать шаблон с псевдонимом для того, чтобы не писать typeid, по какой-то причине не выходит.
+	std::cout << typeid(function_t < double(int) >).name() << std::endl;
 	return 0;
 }
