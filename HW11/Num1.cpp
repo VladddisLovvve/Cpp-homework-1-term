5db63814407770c2d8ead86206a2fbd4097bbd46
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 20:33:31 +0300

    Update Num1.cpp

--- HW11/Num1.cpp
+++ HW11/Num1.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
+#include <typeinfo>
 
 
-
-//true_type и false_type
-template < typename T, T v >
-struct integral_constant {
-    static constexpr T value = v;
-
-    using value_type = T;
-    using type = integral_constant;
-
-    constexpr operator value_type() const 
-    { return value; }
-
-    constexpr value_type operator()() const 
-    { return value; }
-};
-
-template <bool v>
-using bool_constant = integral_constant < bool, v >;
-
-using true_type = bool_constant < true >;
-using false_type = bool_constant < false >;
-
-
-
-//st_function
 template < typename RT, typename ... A >
 struct is_function : std::false_type {};
 
@@ -45,11 +21,26 @@ struct is_function < RT(A...)&& > : std::true_type {};
 template < typename RT >
 inline constexpr bool is_function_v = is_function < RT > ::value;
 
+template < typename RT >
+struct function_type 
+{
+	using type = RT;
+};
+
+template <typename RT>
+using function_t = typename function_type < RT > ::type;
+
+//using function_t = typeid(typename function_type < RT > ::type).name(); - такая реализация почему-то не компилируется
+
+void f() {}
+double g() { return 1.0; }
+
+
 
 
 int main()
 {
 	std::cout << is_function_v < int(bool) > << std::endl;
-	std::cout << is_function_v < double(int) > << std::endl;
+	std::cout << typeid(function_t < double(int) >).name() << std::endl; //реализовать шаблон с псевдонимом для того, чтобы не писать typeid, по какой-то причине не выходит.
 	return 0;
 }
