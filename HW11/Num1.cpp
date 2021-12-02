62821d1dae40a702e06363bd5ee7b2938fb3e9d7
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 18:55:00 +0300

    Update Num1.cpp

--- HW11/Num1.cpp
+++ HW11/Num1.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
-#include <type_traits>
 
 
+
+//true_type и false_type
+template < typename T, T v >
+struct integral_constant {
+    static constexpr T value = v;
+
+    using value_type = T;
+    using type = integral_constant;
+
+    constexpr operator value_type() const 
+    { return value; }
+
+    constexpr value_type operator()() const 
+    { return value; }
+};
+
+template <bool v>
+using bool_constant = integral_constant < bool, v >;
+
+using true_type = bool_constant < true >;
+using false_type = bool_constant < false >;
+
+
+
+//st_function
 template < typename RT, typename ... A >
 struct is_function : std::false_type {};
 
@@ -18,8 +42,8 @@ template < typename RT, typename ... A >
 struct is_function < RT(A...)&& > : std::true_type {};
 
 
-template < typename RT, typename ... A >
-inline constexpr bool is_function_v = is_function < RT, typename ... A > ::value; // почему это не работает с typename ... A???? Без него код компилируется
+template < typename RT >
+inline constexpr bool is_function_v = is_function < RT > ::value;
 
 
 
