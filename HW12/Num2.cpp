994357fba67140f37e9e0af6b75acfe09b7a7087
VladddisLovvve <81115789+VladddisLovvve@users.noreply.github.com>
2021-12-02 23:54:04 +0300

    Update Num2.cpp

--- HW12/Num2.cpp
+++ HW12/Num2.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+
+template < int n >
+struct Fib
+{ static const int Fib < n - 1 > + Fib < n - 2 >; };
+
+
+template < >
+struct Fib < 1 >
+{ static const int value = 1; };
+
+
+template < >
+struct Fib < 2 >
+{ static const int value = 1; };
